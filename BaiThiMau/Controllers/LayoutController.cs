4f44abb6afbb1fc9cbefc1735ca08b374a7229f6
Hai Lit <hailit2306@gmail.com>
2020-05-19 18:52:40 +0700

    oke

---
+++ BaiThiMau/Controllers/LayoutController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiThiMau.Models.Entity;
+using System.Data.Entity;
+
+namespace BaiThiMau.Controllers
+{
+    public class LayoutController : Controller
+    {
+        QLVaLi db = new QLVaLi();
+        // GET: Layout
+        public ActionResult Index()
+        {
+            return View(db.tDanhMucSPs);
+        }
+        public ActionResult CreateMenu()
+        {
+            return PartialView(db.tQuocGias);
+        }
+        public ActionResult GetItem(string manuoc) 
+        {
+            return View("Index", db.tDanhMucSPs.Where(p => p.MaNuocSX == manuoc));
+        }
+        public ActionResult LoadDetails(string id)
+        {
+            return View(db.tDanhMucSPs.Where(p=> p.MaSP.Equals(id)).First());
+        }
+        [HttpPost]
+        public ActionResult DeleteProduct(string id,int ?linhtinh)
+        {
+            db.tDanhMucSPs.Remove(db.tDanhMucSPs.Where(p => p.MaSP.Equals(id)).First());
+            db.SaveChanges();
+            return View("Index",db.tDanhMucSPs);
+        }
+        public ActionResult DeleteProduct(string id)
+        {
+            
+            return View(db.tDanhMucSPs.Where(p => p.MaSP.Equals(id)).First());
+        }
+        public ActionResult EditProduct(string id)
+        {
+            ViewBag.MaChatLieu = new SelectList(db.tChatLieux, "MaChatLieu", "ChatLieu");
+            ViewBag.MaNuocSX = new SelectList(db.tQuocGias, "MaNuoc", "TenNuoc");
+            ViewBag.MaHangSX = new SelectList(db.tHangSXes, "MaHangSX", "HangSX");
+            ViewBag.MaDT = new SelectList(db.tLoaiDTs, "MaDT", "TenLoai");
+            ViewBag.MaLoai = new SelectList(db.tLoaiSPs, "MaLoai", "Loai");
+            ViewBag.MaKichThuoc = new SelectList(db.tKichThuocs, "MaKichThuoc", "KichThuoc");
+
+            return View(db.tDanhMucSPs.Where(p => p.MaSP.Equals(id)).First());
+        }
+        [HttpPost]
+        public ActionResult EditProduct(tDanhMucSP sp)
+        {
+            db.Entry(sp).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult CreateProduct()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.tChatLieux, "MaChatLieu", "ChatLieu");
+            ViewBag.MaNuocSX = new SelectList(db.tQuocGias, "MaNuoc", "TenNuoc");
+            ViewBag.MaHangSX = new SelectList(db.tHangSXes, "MaHangSX", "HangSX");
+            ViewBag.MaDT = new SelectList(db.tLoaiDTs, "MaDT", "TenLoai");
+            ViewBag.MaLoai = new SelectList(db.tLoaiSPs, "MaLoai", "Loai");
+            ViewBag.MaKichThuoc = new SelectList(db.tKichThuocs, "MaKichThuoc", "KichThuoc");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProduct(tDanhMucSP sp)
+        {
+            db.tDanhMucSPs.Add(sp);
+            db.SaveChanges();
+            return View("Index", db.tDanhMucSPs);
+        }
+
+
+
+    }
+}
---
+++ BaiThiMau/Models/Entity/QLVaLi.cs
@@ -0,0 +1,141 @@
+namespace BaiThiMau.Models.Entity
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class QLVaLi : DbContext
+    {
+        public QLVaLi()
+            : base("name=QLVaLi")
+        {
+        }
+
+        public virtual DbSet<tAnhSP> tAnhSPs { get; set; }
+        public virtual DbSet<tChatLieu> tChatLieux { get; set; }
+        public virtual DbSet<tDanhMucSP> tDanhMucSPs { get; set; }
+        public virtual DbSet<tHangSX> tHangSXes { get; set; }
+        public virtual DbSet<tKichThuoc> tKichThuocs { get; set; }
+        public virtual DbSet<tLoaiDT> tLoaiDTs { get; set; }
+        public virtual DbSet<tLoaiSP> tLoaiSPs { get; set; }
+        public virtual DbSet<tQuocGia> tQuocGias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<tAnhSP>()
+                .Property(e => e.MaSP)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tAnhSP>()
+                .Property(e => e.TenFileAnh)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tChatLieu>()
+                .Property(e => e.MaChatLieu)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaSP)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaChatLieu)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaKichThuoc)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaHangSX)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaNuocSX)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaDacTinh)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.Website)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaLoai)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.MaDT)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .Property(e => e.Anh)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tDanhMucSP>()
+                .HasMany(e => e.tAnhSPs)
+                .WithRequired(e => e.tDanhMucSP)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<tHangSX>()
+                .Property(e => e.MaHangSX)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tHangSX>()
+                .Property(e => e.MaNuocThuongHieu)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tKichThuoc>()
+                .Property(e => e.MaKichThuoc)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tKichThuoc>()
+                .Property(e => e.KichThuoc)
+                .IsFixedLength();
+
+            modelBuilder.Entity<tLoaiDT>()
+                .Property(e => e.MaDT)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tLoaiSP>()
+                .Property(e => e.MaLoai)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tQuocGia>()
+                .Property(e => e.MaNuoc)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tQuocGia>()
+                .HasMany(e => e.tDanhMucSPs)
+                .WithOptional(e => e.tQuocGia)
+                .HasForeignKey(e => e.MaNuocSX);
+
+            modelBuilder.Entity<tQuocGia>()
+                .HasMany(e => e.tHangSXes)
+                .WithOptional(e => e.tQuocGia)
+                .HasForeignKey(e => e.MaNuocThuongHieu);
+        }
+    }
+}
